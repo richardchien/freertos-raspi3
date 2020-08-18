1b2e13bca249bba81f6254e3a9db3cae29edce64
Richard Chien <richardchienthebest@gmail.com>
2020-08-18 18:29:51 +0800

    hello ok

--- Demo/main.c
+++ Demo/main.c
@@ -31,11 +31,12 @@ void task2(void *pParam) {
 void main (void)
 {
 	uart_init();
+	uart_puts("hello");
 
-	xTaskCreate(task1, "LED_0", 128, NULL, 0, NULL);
-	xTaskCreate(task2, "LED_1", 128, NULL, 0, NULL);
+	// xTaskCreate(task1, "LED_0", 128, NULL, 0, NULL);
+	// xTaskCreate(task2, "LED_1", 128, NULL, 0, NULL);
 
-	vTaskStartScheduler();
+	// vTaskStartScheduler();
 
 	/*
 	 *	We should never get here, but just in case something goes wrong,
