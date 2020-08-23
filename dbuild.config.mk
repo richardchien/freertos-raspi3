a781e82bc2b70a6083f19f36f46e7ca64bb3a83c
Richard Chien <richardchienthebest@gmail.com>
2020-08-23 22:27:12 +0800

    fix cflags

--- dbuild.config.mk
+++ dbuild.config.mk
@@ -1,7 +1,7 @@
 #ASFLAGS += -mcpu=cortex-a8
 ASFLAGS += -march=armv8-a
 #CFLAGS += -mcpu=cortex-a8 -g -Wall -w
-CFLAGS += -march=armv8-a -g -Wall -w
+CFLAGS += -march=armv8-a -g -Wall -w -nostdlib -nostartfiles -ffreestanding
 #-Wextra
 
 CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/ARM_CA53_64_RASPI3/
