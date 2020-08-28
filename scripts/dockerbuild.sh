21683310931c5cbf44e4f64da96602efd594b3ae
Richard Chien <richardchienthebest@gmail.com>
2020-08-28 16:43:51 +0800

    move dockerfile to separate repo

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/chos -w /chos richardchien/freertos-raspi3-builder:v1.0 bash -- ./scripts/build.sh
+docker run -it --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/chos -w /chos richardchien/chcore_virt_dev:v1.0 bash -- ./scripts/build.sh
